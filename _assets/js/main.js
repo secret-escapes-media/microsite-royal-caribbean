c4627c175e205f5b19e79e72cfdb6cb867901e89
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-09-21 16:18:48 +0100

    tracking pixels and urls

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,11 @@
 // general js for the project that wouldn't be a reuseable component
+
+
+// cachebuster for tracking pixels
+var randomNumber = Math.random() + "";
+randomNumber = parseInt(randomNumber * 10000000000000);
+$('.js-tracking-pixels-cachebusters').children().each(function() {
+  var pixelSrc = $(this).attr('src');
+  var newPixelSrc = pixelSrc.replace(/\[CACHEBUSTER\]/g, randomNumber);
+  $(this).attr('src', newPixelSrc);
+});
